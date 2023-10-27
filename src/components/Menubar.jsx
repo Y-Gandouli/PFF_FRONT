a7bcc324f90a70989170d14314a42eaea99a8f2b
Y-Gandouli <elgandouli.youssef@gmail.com>
2023-10-27 18:08:40 +0100

    first

---
+++ src/components/Menubar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Menubar (props){
+    return (
+        <ul>
+
+  <li className="nav__item"><a href="#"> Client managment</a></li>
+  <li className="nav__item"><a href="#"> File uploads</a></li>
+  
+</ul>
+    );
+}
+
+export default Menubar;
