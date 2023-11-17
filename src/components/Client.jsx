e2cd6a8268e54b0b2feecdc33ff46edd209ab388
soufiane El Kahlaoui <s.elkahlaoui007@gmail.com>
2023-11-17 03:40:37 +0100

    https

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -66,7 +66,7 @@ function Client() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     await customFetch({
-      path: "http://52.154.77.66:80/clientsManagement",
+      path: "https://52.154.77.66:80/clientsManagement",
       method: "POST",
       bodyData: data,
     }).then((data) => {
@@ -75,7 +75,7 @@ function Client() {
   };
   useEffect(() => {
     customFetch({
-      path: "http://52.154.77.66:80/clientsManagement",
+      path: "https://52.154.77.66:80/clientsManagement",
       method: "GET",
     }).then((clients) => {
       setClients(clients);
@@ -104,7 +104,7 @@ function Client() {
     console.log(JSON.stringify({ ids: selectedRows }));
 
     customFetch({
-      path: "http://52.154.77.66:80/clientsManagement",
+      path: "https://52.154.77.66:80/clientsManagement",
       method: "DELETE",
       bodyData: { ids: selectedRows },
     }).then(() => {
@@ -115,7 +115,7 @@ function Client() {
     console.log(JSON.stringify(selectedRows[0]));
 
     customFetch({
-      path: `http://52.154.77.66:80/clientsManagement/${selectedRows[0]}`,
+      path: `https://52.154.77.66:80/clientsManagement/${selectedRows[0]}`,
       method: "PUT",
       bodyData: data,
     }).then(() => {
