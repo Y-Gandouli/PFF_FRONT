611752739ffb84f99bb72c17f83165d0064b2005
soufiane El Kahlaoui <s.elkahlaoui007@gmail.com>
2023-11-17 01:16:04 +0100

    edit url endpoint

--- src/components/FilesUpload.jsx
+++ src/components/FilesUpload.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from "@mui/material/styles";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 function FilesUpload() {
-  return <div>i'm FilesUpload page</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "50vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload file
+        <VisuallyHiddenInput type="file" />
+      </Button>
+    </div>
+  );
 }
 
 export default FilesUpload;
